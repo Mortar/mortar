882c90097c3d58e2c77f7dd03bf70e64dd780fdd
chris <chris@ac150577-6901-3246-a117-cd8547e6756f>
2008-02-07 23:01:24 +0000

    re-sketch and hack import :-S

--- __init__.py
+++ __init__.py
@@ -1,23 +1,26 @@
+# Copyright (c) 2007-2008 Simplistix Ltd
+#
+# All rights reserved.
+
 from content import Content
 from interfaces import IContent
 
-def search(*args,**kw):
-    "returns an IResultSet, spec allows searching, sorting, etc"
-    pass
+def content():
+    return Content()
 
-def setRoot():
-    "sets the root IContent object"
-    
-def getRoot():
-    "returns the root as an IContent"
-    pass
+# bleugh
+import convertors
 
-def getById(id):
-    "returns the IContent specified by the id"
-    pass
+## def search(*args,**kw):
+##     "returns an IResultSet, spec allows searching, sorting, etc"
+##     pass
 
-def new(type):
-    "return a new IContent"
-    if not IContent.isImplementedBy(type):
-        type = getById(type)
-    return Content(type)
+## def getById(id):
+##     "returns the IContent specified by the id"
+##     pass
+
+## def new(type):
+##     "return a new IContent"
+##     if not IContent.isImplementedBy(type):
+##         type = getById(type)
+##     return Content(type)
