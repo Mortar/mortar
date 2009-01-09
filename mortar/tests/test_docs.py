6947ca61b267cae1ec5860ba39b50097e66f2d8d
chris <chris@ac150577-6901-3246-a117-cd8547e6756f>
2009-01-09 11:22:45 +0000

    - remove crufty traversing.txt - pretty up test_docs.py

--- mortar/tests/test_docs.py
+++ mortar/tests/test_docs.py
@@ -1,23 +1,14 @@
 # Copyright (c) 2007-2008 Simplistix Ltd
 # See license.txt for license details.
 
-import os
 import unittest
 from glob import glob
+from os.path import dirname,join,pardir
 from zope.testing.doctest import DocFileSuite, REPORT_NDIFF,ELLIPSIS
 
 def test_suite():
-    suite = unittest.TestSuite()
-    for path in glob(os.path.join(os.path.dirname(__file__),'..','docs','*.txt')):
-        # BODGE!
-        if os.path.split(path)[-1] in ('traversing.txt',):
-            continue
-        # work around zope.testing bug
-        path = '..'+path.split('..')[1]
-        suite.addTest(
-            DocFileSuite(path, optionflags=REPORT_NDIFF|ELLIPSIS)
-            )
-    return suite
-
-if __name__ == '__main__':
-    unittest.main(default='test_suite')
+    return DocFileSuite(
+        optionflags=REPORT_NDIFF|ELLIPSIS,
+        *glob(join(dirname(__file__),pardir,'docs','*.txt'))
+        )
+        
